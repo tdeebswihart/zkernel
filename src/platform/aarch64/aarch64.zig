793fad43b9a1ca8dd74e48f6d76a31e6de0b42bd
Tim Deeb-Swihart <tim@deebswih.art>
2021-05-28 08:32:16 -0400

    RM registers from the aarch package

--- src/platform/aarch64/aarch64.zig
+++ src/platform/aarch64/aarch64.zig
@@ -1,11 +1,9 @@
-pub const registers = @import("registers.zig");
 pub const cpu = @import("cpu.zig");
 pub const memory = @import("memory.zig");
 
 comptime {
     _ = cpu;
     _ = memory;
-    _ = registers;
 }
 
 pub fn init() void {
